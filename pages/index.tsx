164d23d6183ec3c0634914f211f82d6fb9ce0ec7
eugeneffects <eugeneffects@gmail.com>
2022-02-04 17:15:56 +0900

    editing:tailwinds

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <h1 className='text-xl font-bold'>안녕하세요.</h1>
+    <div className='flex justify-center'>
+      <h1 className='text-xl font-bold '>안녕하세요.</h1>
+    </div>
   )
 }
 
